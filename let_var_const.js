8590a1d65b52fa6e86bae5fc92950017b83c3ea3
Sanaha Pathan <sanaha.pathan@gmail.com>
2022-02-17 10:31:59 +0530

    code updated

---
+++ let_var_const.js
@@ -0,0 +1,29 @@
+// "use strict";
+// use strich is going to check the scope and type of data compulsory 
+var test1 = "CTS Pune";
+let test2 = 'CTS Mumbai';
+const test3 = 'Angular';
+var test1 = "Claritech Pune";
+test1 ="Mumbai";
+//  test2 = "Zest";
+// console.log(test2);
+//  test3 ='Java Spring';
+// console.log(test3);
+function func(){
+    var test5 =" Hello JavaScript";
+    const test6 =" Perl";
+    
+    for (let i = 0; i <5; i++) {
+        // var test6 = "Hello JS";
+        // console.log(i);
+        // console.log(test3);
+        // console.log(test1);
+         console.log(test6);        
+    }
+   console.log(test6);
+}
+func();
+ console.log(test6);
+
+
+test4 ="CTS Hydrabad";
